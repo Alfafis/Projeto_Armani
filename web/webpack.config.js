c4ac6147d6a0307030d59ae7f84903dbcb6ad679
Alfafis <ctpmmg2009_101@hotmail.com>
2023-06-20 20:13:59 -0300

    fixed

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "./src"),
+    path: path.resolve(__dirname, "./dist"),
     chunkFilename: "[id].[contenthash].js",
   },
   module: {
